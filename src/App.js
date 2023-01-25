49c6cab0e49581f206b5c186a753ac47c95b95b2
Keyvan <95883365+keyvanone@users.noreply.github.com>
2023-01-25 21:08:31 +0330

    adding react-router-dom to App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,71 @@
-import React, { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import React from 'react';
 
-import feedbackData from './components/feedbackData';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+// import { FaQuestion } from 'react-icons/fa';
+
 import FeedbackForm from './components/FeedbackForm';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import Header from './components/Header';
+import About from './pages/About';
+import { FeedbackProvider } from './context/FeedbackContext';
 
 function App(deleteHandler) {
-  const [feedback, setFeedback] = useState(feedbackData);
-
-  const deleteFeedback = (id) => {
-    setFeedback(feedback.filter((item) => item.id !== id));
-  };
-
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
-  };
-
   return (
-    <div
-      className="relative
+    <FeedbackProvider>
+      <div
+        className="relative
       flex  
       justify-center
       min-h-screen
+      max-w-1600
       overflow-hidden
       bg-sky-300
       font-semibold
     "
-    >
-      <div>
-        <Header />
-        <FeedbackForm handleAdd={addFeedback} feedback={feedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} deleteHandler={deleteFeedback} />
+      >
+        <div>
+          <BrowserRouter>
+            <Routes>
+              <Route
+                path="/"
+                exact
+                element={
+                  <>
+                    <Header />
+                    <FeedbackForm />
+                    <FeedbackStats />
+                    <FeedbackList />
+                  </>
+                }
+              />
+
+              <Route
+                className="relative
+                  flex
+                justify-center
+                min-h-screen
+                max-w-1600
+                overflow-hidden
+                bg-sky-300
+                font-semibold
+              "
+                path="/about"
+                element={
+                  <div>
+                    <Header />
+                    <About />
+                  </div>
+                }
+              />
+            </Routes>
+            {/* <Link to={'/'}>
+            <FaQuestion className="relative items-center text-purple-500 translate-x-44 translate-y-36" />
+          </Link> */}
+          </BrowserRouter>
+        </div>
       </div>
-    </div>
+    </FeedbackProvider>
   );
 }
 

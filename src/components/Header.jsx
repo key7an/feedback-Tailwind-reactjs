d68d9f61049e9777e85f65a4fa97e3eb47beb671
Keyvan <95883365+keyvanone@users.noreply.github.com>
2023-01-26 12:20:34 +0330

    link added to title
    no problem

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 // import PropTypes from 'prop-types';
 
 const Header = () => {
   return (
     <div
-      className="relative flex justify-center items-center text-center mb-10 p-2
+      className="relative flex flex-col justify-center items-center text-center mb-10 p-2
        top-0 text-2xl border-b-2 border-cyan-600 text-cyan-700 font-bold"
     >
-      Feedback UI
+      <Link to="/">Feedback UI</Link>
     </div>
   );
 };

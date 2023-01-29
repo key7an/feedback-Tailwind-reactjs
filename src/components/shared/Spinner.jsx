ac2e2b8dfde3e8eabfa2d54ed1d6cc94741b2d40
Keyvan <95883365+keyvanone@users.noreply.github.com>
2023-01-29 22:50:44 +0330

    spinner files added
    spinner gif and related codes to it

---
+++ src/components/shared/Spinner.jsx
@@ -0,0 +1,7 @@
+import spinner from '../assets/spinner.gif';
+
+const Spinner = () => {
+  return <img src={spinner} alt="spinner gif" className=" w-32 m-auto block" />;
+};
+
+export default Spinner;
